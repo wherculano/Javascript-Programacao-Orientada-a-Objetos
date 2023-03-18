83c7b158dd404c95a888e471bb484444c4a29602
wagner <wagner.santos@accurate.com.br>
2023-03-17 22:38:10 -0300

    Adicionado novo atributo, getters e setters

--- aula_005_User.js
+++ aula_005_User.js
@@ -1,30 +1,55 @@
 export default class User {
-  #nome  // forma atual de declarar um atributo privado (antes era usando _, como por exemplo, _nome)
-  #email
-  #nascimento
-  #role
-  #ativo
-  constructor(nome, email, nascimento, role, ativo = true) {
+  #nome; // forma atual de declarar um atributo privado (antes era usando _, como por exemplo, _nome)
+  #sobrenome;
+  #email;
+  #nascimento;
+  #role;
+  #ativo;
+  constructor(nome, sobrenome, email, nascimento, role, ativo = true) {
     this.#nome = nome;
+    this.#sobrenome = sobrenome;
     this.#email = email;
     this.#nascimento = nascimento;
     this.#role = role || "estudante"; // valor recebido OU estudante
     this.#ativo = ativo;
   }
 
-  #montarObjUser(){
-    return ({
-      nome: this.#nome,
-      email: this.#email,
-      nascimento: this.#nascimento,
-      role: this.#role,
-      ativo: this.#ativo
-    })
+  get nome() {
+    return `${this.#nome} ${this.#sobrenome}`;
+  }
+
+  set nome(novoNome) {
+    if (novoNome === "") {
+      throw new Error("Nome não pode estar em branco");
+    }
+    let [nome, ...sobrenome] = novoNome.split(" ");
+    sobrenome = sobrenome.join(" ");
+    this.#nome = nome;
+    this.#sobrenome = sobrenome;
+  }
+
+  get sobrenome() {
+    return this.#sobrenome;
+  }
+
+  get email() {
+    return this.#email;
+  }
+
+  get nascimento() {
+    return this.#nascimento;
+  }
+
+  get role() {
+    return this.#role;
+  }
+
+  get ativo() {
+    return this.#ativo;
   }
 
   exibirInfos() {
-    const objUser = this.#montarObjUser()
-    return `${objUser.nome}, ${objUser.email}, ${objUser.nascimento}, ${objUser.role}, ${objUser.ativo}`;
+    return `${this.nome}, ${this.email}, ${this.nascimento}, ${this.role}, ${this.ativo}`;
   }
 }
 
