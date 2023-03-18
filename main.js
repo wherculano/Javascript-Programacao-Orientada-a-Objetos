ca545796cab09840faee9dc46236f7936599ee51
wagner <wagner.santos@accurate.com.br>
2023-03-17 22:38:40 -0300

    Utilizando getters e setters

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ import User from "./aula_005_User.js";
 import Docente from "./aula_005_Docente.js";
 import Admin from "./aula_005_Admin.js";
 
+const user = new User("Matheus", "Herculano", "math@eus.com", "2020-01-01");
+console.log(user.exibirInfos());
+console.log(user.nome);
 
-const user = new User("Matheus", "math@eus.com", "2020-01-01")
-console.log(user.exibirInfos())
+console.log("-=".repeat(20));
 
-// user.nome = "Wagner" // Ok, pois está sendo criado um novo atributo apenas para esta instancia
-// user.#nome = "Wagner" // Não pode, pois está tentando alterar um atributo privado
+const admin = new Admin("Wagner", "Herculano", "w@w.com", "2023-01-01");
+console.log(admin.exibirInfos());
+console.log(`\nNome admin: ${admin.nome}`);
+console.log(`Sobrenome admin: ${admin.sobrenome}`);
+
+admin.nome = "João José Emiliano";
+console.log(`\nNome admin: ${admin.nome}`);
+console.log(`Sobrenome admin: ${admin.sobrenome}`);
