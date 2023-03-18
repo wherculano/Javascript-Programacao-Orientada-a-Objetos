2c99b67dfe1f729919928c04050abb68e650c1a9
wagner <wagner.santos@accurate.com.br>
2023-03-17 22:37:16 -0300

    Adicionado novo atributo

--- aula_005_Admin.js
+++ aula_005_Admin.js
@@ -1,7 +1,14 @@
 import User from "./aula_005_User.js";
 
 export default class Admin extends User {
-  constructor(nome, email, nascimento, role = "admin", ativo = true) {
+  constructor(
+    nome,
+    sobrenome,
+    email,
+    nascimento,
+    role = "admin",
+    ativo = true
+  ) {
     super(nome, email, nascimento, role, ativo);
   }
   criarCurso(nomeDoCurso, numVagas) {
--- aula_005_Docente.js
+++ aula_005_Docente.js
@@ -1,7 +1,14 @@
 import User from "./aula_005_User.js";
 
 export default class Docente extends User {
-  constructor(nome, email, nascimento, role = "docente", ativo = true) {
+  constructor(
+    nome,
+    sobrenome,
+    email,
+    nascimento,
+    role = "docente",
+    ativo = true
+  ) {
     super(nome, email, nascimento, role, ativo);
   }
 
